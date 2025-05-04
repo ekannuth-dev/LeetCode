5a3132936e6987f7132f16a7c47e9ddc97049391
ekannuth-dev <ekannuth@uwaterloo.ca>
2025-05-04 17:02:06 -0400

    Did not solve Word Search

--- random1.py
+++ random1.py
@@ -1,11 +1,2 @@
-upperBound = 0
-lowerBound = 1
-ranges = [[lowerBound, upperBound]]
-lowerBound = upperBound
-def func(strings):
-	s = "banana"
-	s = list(s)
-	print(s)
-nums = [1,2,3]
-print(nums[:1])
-func("zadbce")
+sampleString = "abcdde"
+sampleString.pop()
